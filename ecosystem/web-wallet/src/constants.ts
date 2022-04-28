b607df9915ac217bac7d0b918e32dccef2598e38
Kent White <kent@aptoslabs.com>
2022-04-28 14:51:04 -0700

    [web-wallet] Add scripts to pages for dapps

--- ecosystem/web-wallet/src/constants.ts
+++ ecosystem/web-wallet/src/constants.ts
@@ -4,3 +4,4 @@
 export const KEY_LENGTH: number = 64
 export const devnetNodeUrl = 'https://fullnode.devnet.aptoslabs.com'
 export const faucetUrl = 'https://faucet.devnet.aptoslabs.com'
+export const walletStateLocalStorageKey = 'aptosWalletState'
